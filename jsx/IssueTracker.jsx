5a555a29261e19cf53f4f2f256a1db58504edebf
KaushalKumarDesai <kdesai8563@conestogac.on.ca>
2024-03-11 08:23:33 -0400

    classed structure of IssueTracker

---
+++ jsx/IssueTracker.jsx
@@ -0,0 +1,42 @@
+class IssueFilter extends React.Component{
+    render() {
+        return(
+            <div>This is placeholder for the issue filter.</div>
+        );
+    }
+}
+
+class IssueTable extends React.Component{
+    render() {
+        return(
+            <div>This is placeholder for the issue Table.</div>
+        );
+    }
+}
+
+class IssueAdd extends React.Component{
+    render() {
+        return(
+            <div>This is placeholder for adding the Issue.</div>
+        );
+    }
+}
+
+class IssueTracker extends React.component{
+    render() {
+        return (
+            <React.Fragment>
+                <h1>Issue Tracker</h1>
+                <IssueFilter />
+                <hr />
+                <IssueTable />
+                <hr />
+                <IssueAdd />
+            </React.Fragment>
+        );
+    }
+}
+
+const element = <IssueTracker />;
+
+ReactDOM.render(element,document.getElementById('contents'));
