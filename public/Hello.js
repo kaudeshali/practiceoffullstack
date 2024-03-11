9626443cbcaf9cc39e8651d5bdd17ea11f07cdab
KaushalKumarDesai <kdesai8563@conestogac.on.ca>
2024-03-10 21:43:59 -0400

    npx babel src --presets @babel/react --out-dir public to send .js file into public folder

---
+++ public/Hello.js
@@ -0,0 +1,4 @@
+const element = /*#__PURE__*/React.createElement("div", {
+  title: "Outer dev"
+}, /*#__PURE__*/React.createElement("h1", null, "Hello World. From Kaushal!!"));
+ReactDOM.render(element, document.getElementById("contents"));
